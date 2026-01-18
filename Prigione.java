023490227cdce1447dbfc75684b128b02d1ff88a
Michelle1711 <piasentiermichelle@gmail.com>
2026-01-18 18:34:56 +0100

    Modifica Tabellone e Prigione

--- Prigione.java
+++ Prigione.java
@@ -1,10 +1,14 @@
 public class Prigione extends Casella {
     int turni;
 
-    public Prigione(int turni){
-        this.turni = turni;
-    }
+    public Prigione(String nome, Casella successiva, Casella precedente, int numeroCasella) {
+        super(nome, successiva, precedente, numeroCasella);
+        this.turni = 0;
+    }    
+
     public String toString(){
-        return "Prigione: " + turni + " turni rimanenti.";
+        return "Prigione{" +
+                "turni=" + turni +
+                "} " + super.toString();
     }
 }
