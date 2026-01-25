a7101e92a61e176efcf45f624e99d1ce7bee47bb
Michelle1711 <piasentiermichelle@gmail.com>
2026-01-25 21:55:19 +0100

    Update Partita.java

--- Partita.java
+++ Partita.java
@@ -124,6 +124,31 @@ public class Partita{
                     }
                 }
             }
+        }else if(casellaCorrente instanceof Societa){
+            System.out.println(giocatore.getNome() + " è atterrato su una Società.");
+            if(((Societa) casellaCorrente).getProprietario()==null){
+                System.out.println("Questa società è disponibile per l'acquisto.");
+                System.out.println("Vuoi acquistare questa società? (s/n)");
+                String risposta = Leggi.unoString();
+                if(risposta.equalsIgnoreCase("s")){
+                    Societa societa = (Societa) casellaCorrente;
+                    if(giocatore.acquistaSocieta(societa)){
+                        System.out.println(giocatore.getNome() + " ha acquistato " + societa.getNome());
+                    }else{
+                        System.out.println(giocatore.getNome() + " non ha abbastanza denaro per acquistare " + societa.getNome());
+                    }
+                }
+            }else{
+                Giocatore proprietario = ((Societa) casellaCorrente).getProprietario();
+                if(proprietario != giocatore){  
+                    int affitto = ((Societa) casellaCorrente).affitto();
+                    System.out.println("Questa società è di proprietà di " + proprietario.getNome() + ". Deve pagare un affitto di " + affitto);
+                    if(!giocatore.pagaAffitto(proprietario, affitto)){
+                        System.out.println(giocatore.getNome() + " non ha abbastanza denaro per pagare l'affitto!");
+                        gestisciBancarotta(giocatore);
+                    }
+                }
+            }
         }else if(casellaCorrente instanceof Casella){
             System.out.println(giocatore.getNome() + " è atterrato su " + casellaCorrente.getNome());
         }
