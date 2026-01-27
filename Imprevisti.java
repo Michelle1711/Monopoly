296d73763163a6679d3666c54c378f6652482a5d
Michelle1711 <piasentiermichelle@gmail.com>
2026-01-27 22:33:43 +0100

    Update Imprevisti.java

--- Imprevisti.java
+++ Imprevisti.java
@@ -8,7 +8,7 @@ public class Imprevisti extends Casella {
         mazzoImprevisti = new String[] {
                 "Paga una multa di 50",
                 "Vai in prigione",
-                "Paga 100€ per tasse arretrate",
+                "Paga 100 per tasse arretrate",
                 "Avanza di 3 caselle",
                 "Riparazioni stradali: paga 40",
                 "Multa per eccesso di velocità: paga 300"
