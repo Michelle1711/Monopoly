4205067516e700950355b498fd571343271f02e3
Michelle1711 <piasentiermichelle@gmail.com>
2026-01-27 19:41:44 +0100

    Update Tabellone.java

--- Tabellone.java
+++ Tabellone.java
@@ -8,54 +8,61 @@ public class Tabellone{
     }
 
     private void inizializzaTabellone(){
-        tabellone[0] = new Casella("VIA!",tabellone[1],tabellone[39],1);
+        // PRIMA PASS: Creazione di tutte le caselle senza il linking
+        tabellone[0] = new Casella("VIA!", null, null, 1);
+        
         //blocco Marrone e Azzurro
-        tabellone[1] = new Terreno("Vicolo Corto",tabellone[2],tabellone[0], 2, 60,Colore.MARRONE,null);
-        tabellone[2] = new Probabilita("Probabilità",tabellone[3],tabellone[1],3);
-        tabellone[3] = new Terreno("Vicolo Stretto",tabellone[4],tabellone[2],4,60,Colore.MARRONE,null);
-        tabellone[4] = new Tasse("Tassa sul reddito",tabellone[5],tabellone[3],5,200);
-        tabellone[5] = new StazioneTreno("Stazione Sud",tabellone[6],tabellone[4],6);
-        tabellone[6] = new Terreno("Bastioni Gran Sasso",tabellone[7],tabellone[5],7,100,Colore.AZZURRO,null);
-        tabellone[7] = new Imprevisti("Imprevisti",tabellone[8],tabellone[6],8);
-        tabellone[8] = new Terreno("Viale Monterosa",tabellone[9],tabellone[7],9,100,Colore.AZZURRO,null);
-        tabellone[9] = new Terreno("Viale Vesuvio",tabellone[10],tabellone[8],10,120,Colore.AZZURRO,null);
+        tabellone[1] = new Terreno("Vicolo Corto", null, null, 2, 60, Colore.MARRONE, null);
+        tabellone[2] = new Probabilita("Probabilità", null, null, 3);
+        tabellone[3] = new Terreno("Vicolo Stretto", null, null, 4, 60, Colore.MARRONE, null);
+        tabellone[4] = new Tasse("Tassa sul reddito", null, null, 5, 200);
+        tabellone[5] = new StazioneTreno("Stazione Sud", null, null, 6);
+        tabellone[6] = new Terreno("Bastioni Gran Sasso", null, null, 7, 100, Colore.AZZURRO, null);
+        tabellone[7] = new Imprevisti("Imprevisti", null, null, 8);
+        tabellone[8] = new Terreno("Viale Monterosa", null, null, 9, 100, Colore.AZZURRO, null);
+        tabellone[9] = new Terreno("Viale Vesuvio", null, null, 10, 120, Colore.AZZURRO, null);
 
         // prigione e blocco Rosa e Arancio
-        tabellone[10] = new Prigione("Prigione",tabellone[11],tabellone[9],11);
-        tabellone[11] = new Terreno("Via Accademia",tabellone[12],tabellone[10],12,140,Colore.ROSA,null);
-        tabellone[12] = new Societa("Società Elettrica",tabellone[13],tabellone[11],13);
-        tabellone[13] = new Terreno("Corso Ateneo",tabellone[14],tabellone[12],14,140,Colore.ROSA,null);
-        tabellone[14] = new Terreno("Piazza Università",tabellone[15],tabellone[13],15,160,Colore.ROSA,null);
-        tabellone[15] = new StazioneTreno("Stazione Ovest",tabellone[16],tabellone[14],16);
-        tabellone[16] = new Terreno("Via Verdi",tabellone[17],tabellone[15],17,180,Colore.ARANCIONE,null);
-        tabellone[17] = new Probabilita("Probabilità",tabellone[18],tabellone[16],18);
-        tabellone[18] = new Terreno("Corso Raffaello",tabellone[19],tabellone[17],19,180,Colore.ARANCIONE,null);
-        tabellone[19] = new Terreno("Piazza Dante",tabellone[20],tabellone[18],20,200,Colore.ARANCIONE,null);
+        tabellone[10] = new Prigione("Prigione", null, null, 11);
+        tabellone[11] = new Terreno("Via Accademia", null, null, 12, 140, Colore.ROSA, null);
+        tabellone[12] = new Societa("Società Elettrica", null, null, 13);
+        tabellone[13] = new Terreno("Corso Ateneo", null, null, 14, 140, Colore.ROSA, null);
+        tabellone[14] = new Terreno("Piazza Università", null, null, 15, 160, Colore.ROSA, null);
+        tabellone[15] = new StazioneTreno("Stazione Ovest", null, null, 16);
+        tabellone[16] = new Terreno("Via Verdi", null, null, 17, 180, Colore.ARANCIONE, null);
+        tabellone[17] = new Probabilita("Probabilità", null, null, 18);
+        tabellone[18] = new Terreno("Corso Raffaello", null, null, 19, 180, Colore.ARANCIONE, null);
+        tabellone[19] = new Terreno("Piazza Dante", null, null, 20, 200, Colore.ARANCIONE, null);
 
         // blocco Rosso e Giallo
-        tabellone[20] = new Casella("PARCHEGGIO GRATUITO!",tabellone[21],tabellone[19],21);
-        tabellone[21] = new Terreno("Via Marco Polo",tabellone[22],tabellone[20],22,220,Colore.ROSSO,null);
-        tabellone[22] = new Imprevisti("Imprevisti",tabellone[23],tabellone[21],23);
-        tabellone[23] = new Terreno("Corso Magellano",tabellone[24],tabellone[22],24,220,Colore.ROSSO,null);
-        tabellone[24] = new Terreno("Largo Colombo",tabellone[25],tabellone[23],25,240,Colore.ROSSO,null);
-        tabellone[25] = new StazioneTreno("Stazione Nord",tabellone[26],tabellone[24],26);
-        tabellone[26] = new Terreno("Viale Costantino",tabellone[27],tabellone[25],27,260,Colore.GIALLO,null);
-        tabellone[27] = new Terreno("Viale Traiano",tabellone[28],tabellone[26],28,260,Colore.GIALLO,null);
-        tabellone[28] = new Societa("Società Acqua Potabile",tabellone[29],tabellone[27],29);
-        tabellone[29] = new Terreno("Piazza Giulio Cesare",tabellone[30],tabellone[28],30,280,Colore.GIALLO,null);
+        tabellone[20] = new Casella("PARCHEGGIO GRATUITO!", null, null, 21);
+        tabellone[21] = new Terreno("Via Marco Polo", null, null, 22, 220, Colore.ROSSO, null);
+        tabellone[22] = new Imprevisti("Imprevisti", null, null, 23);
+        tabellone[23] = new Terreno("Corso Magellano", null, null, 24, 220, Colore.ROSSO, null);
+        tabellone[24] = new Terreno("Largo Colombo", null, null, 25, 240, Colore.ROSSO, null);
+        tabellone[25] = new StazioneTreno("Stazione Nord", null, null, 26);
+        tabellone[26] = new Terreno("Viale Costantino", null, null, 27, 260, Colore.GIALLO, null);
+        tabellone[27] = new Terreno("Viale Traiano", null, null, 28, 260, Colore.GIALLO, null);
+        tabellone[28] = new Societa("Società Acqua Potabile", null, null, 29);
+        tabellone[29] = new Terreno("Piazza Giulio Cesare", null, null, 30, 280, Colore.GIALLO, null);
 
         // Vai in Prigione e blocco Verde e Viola
-        tabellone[30] = new Prigione("VAI IN PRIGIONE!",tabellone[31],tabellone[29],31);
-        tabellone[31] = new Terreno("Via Roma",tabellone[32],tabellone[30],32,300,Colore.VERDE,null);
-        tabellone[32] = new Terreno("Corso Impero",tabellone[33],tabellone[31],33,300,Colore.VERDE,null);
-        tabellone[33] = new Probabilita("Probabilità",tabellone[34],tabellone[32],34);
-        tabellone[34] = new Terreno("Largo Augusto",tabellone[35],tabellone[32],35,320,Colore.VERDE,null);
-        tabellone[35] = new StazioneTreno("Stazione Est",tabellone[36],tabellone[34],36);
-        tabellone[36] = new Imprevisti("Imprevisti",tabellone[37],tabellone[35],37);
-        tabellone[37] = new Terreno("Viale dei Giardini",tabellone[38],tabellone[35],38,350,Colore.BLU,null);
-        tabellone[38] = new Tasse("Tassa di lusso", tabellone[39], tabellone[37], 39, 100); // Tassa di lusso
-        tabellone[39] = new Terreno("Parco della Vittoria",tabellone[0],tabellone[38],40,400,Colore.BLU,null);
+        tabellone[30] = new Prigione("VAI IN PRIGIONE!", null, null, 31);
+        tabellone[31] = new Terreno("Via Roma", null, null, 32, 300, Colore.VERDE, null);
+        tabellone[32] = new Terreno("Corso Impero", null, null, 33, 300, Colore.VERDE, null);
+        tabellone[33] = new Probabilita("Probabilità", null, null, 34);
+        tabellone[34] = new Terreno("Largo Augusto", null, null, 35, 320, Colore.VERDE, null);
+        tabellone[35] = new StazioneTreno("Stazione Est", null, null, 36);
+        tabellone[36] = new Imprevisti("Imprevisti", null, null, 37);
+        tabellone[37] = new Terreno("Viale dei Giardini", null, null, 38, 350, Colore.BLU, null);
+        tabellone[38] = new Tasse("Tassa di lusso", null, null, 39, 100);
+        tabellone[39] = new Terreno("Parco della Vittoria", null, null, 40, 400, Colore.BLU, null);
 
+        // SECONDA PASS: Collegamento delle caselle in catena circolare
+        for (int i = 0; i < 40; i++) {
+            tabellone[i].setSuccessiva(tabellone[(i + 1) % 40]);
+            tabellone[i].setPrecedente(tabellone[(i - 1 + 40) % 40]);
+        }
     }
 
     public Casella getCasella(int indice){
